9ca4dcd5e45d768faa8ba379c2cda535cd9d566a
zebs <Student@A414-12>
2025-02-06 19:01:04 -0500

    ICA_ONE

--- ZebCarRental/Form1.Designer.cs
+++ ZebCarRental/Form1.Designer.cs
@@ -42,76 +42,77 @@
             // lblDays
             // 
             lblDays.AutoSize = true;
-            lblDays.Location = new Point(49, 147);
+            lblDays.Location = new Point(49, 167);
             lblDays.Name = "lblDays";
-            lblDays.Size = new Size(128, 15);
-            lblDays.TabIndex = 0;
-            lblDays.Text = "Enter total days rented:";
+            lblDays.Size = new Size(148, 17);
+            lblDays.TabIndex = 3;
+            lblDays.Text = "Enter total da&ys rented:";
             // 
             // lblRate
             // 
             lblRate.AutoSize = true;
-            lblRate.Location = new Point(49, 113);
+            lblRate.Location = new Point(49, 128);
             lblRate.Name = "lblRate";
-            lblRate.Size = new Size(65, 15);
+            lblRate.Size = new Size(78, 17);
             lblRate.TabIndex = 1;
-            lblRate.Text = "Daily Rate: ";
+            lblRate.Text = "Daily Ra&te: ";
             // 
             // txtRate
             // 
-            txtRate.Location = new Point(205, 105);
+            txtRate.Location = new Point(203, 120);
             txtRate.Name = "txtRate";
-            txtRate.Size = new Size(100, 23);
-            txtRate.TabIndex = 3;
+            txtRate.Size = new Size(100, 25);
+            txtRate.TabIndex = 2;
             // 
             // txtDays
             // 
-            txtDays.Location = new Point(205, 139);
+            txtDays.Location = new Point(203, 164);
             txtDays.Name = "txtDays";
-            txtDays.Size = new Size(100, 23);
-            txtDays.TabIndex = 5;
+            txtDays.Size = new Size(100, 25);
+            txtDays.TabIndex = 4;
             // 
             // btnCalc
             // 
-            btnCalc.Location = new Point(49, 388);
+            btnCalc.Location = new Point(49, 440);
             btnCalc.Name = "btnCalc";
-            btnCalc.Size = new Size(75, 23);
-            btnCalc.TabIndex = 6;
+            btnCalc.Size = new Size(75, 26);
+            btnCalc.TabIndex = 5;
             btnCalc.Text = "&Calculate";
             btnCalc.UseVisualStyleBackColor = true;
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(264, 388);
+            btnClear.Location = new Point(264, 440);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(75, 23);
-            btnClear.TabIndex = 7;
+            btnClear.Size = new Size(75, 26);
+            btnClear.TabIndex = 6;
             btnClear.Text = "Clea&r";
             btnClear.UseVisualStyleBackColor = true;
             // 
             // btnQuit
             // 
-            btnQuit.Location = new Point(492, 388);
+            btnQuit.Location = new Point(492, 440);
             btnQuit.Name = "btnQuit";
-            btnQuit.Size = new Size(75, 23);
-            btnQuit.TabIndex = 8;
+            btnQuit.Size = new Size(75, 26);
+            btnQuit.TabIndex = 7;
             btnQuit.Text = "&Quit";
             btnQuit.UseVisualStyleBackColor = true;
             // 
             // lstOut
             // 
             lstOut.FormattingEnabled = true;
-            lstOut.ItemHeight = 15;
-            lstOut.Location = new Point(49, 214);
+            lstOut.ItemHeight = 17;
+            lstOut.Location = new Point(49, 243);
             lstOut.Name = "lstOut";
-            lstOut.Size = new Size(518, 154);
+            lstOut.Size = new Size(518, 174);
             lstOut.TabIndex = 9;
+            lstOut.TabStop = false;
             // 
             // lblForm
             // 
             lblForm.AutoSize = true;
             lblForm.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblForm.Location = new Point(49, 37);
+            lblForm.Location = new Point(49, 42);
             lblForm.Name = "lblForm";
             lblForm.Size = new Size(126, 21);
             lblForm.TabIndex = 10;
@@ -119,9 +120,9 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 510);
             Controls.Add(lblForm);
             Controls.Add(lstOut);
             Controls.Add(btnQuit);
@@ -131,8 +132,10 @@
             Controls.Add(txtRate);
             Controls.Add(lblRate);
             Controls.Add(lblDays);
+            Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.ControlText;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Zebs Whips";
             ResumeLayout(false);
             PerformLayout();
         }
